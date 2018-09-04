6195a1af21e7bc9aaef1c9d6043d9900643e09b5
Sergio Yanez <seryan.1115@gmail.com>
2018-09-03 21:13:30 -0600

    program now working

--- wordCount.py
+++ wordCount.py
@@ -12,8 +12,6 @@ if len(sys.argv) is not 3:
 #grab the first arg file
 fileName = sys.argv[1]
 
-
-
 # check for all files to exist
 if not os.path.exists(fileName):
     print ("file %s does not exists!" % fileName)
@@ -24,33 +22,21 @@ wordList  = {}
 
 # open file
 with open(fileName,"r") as inputFile:
-    for line in inputFile:                  # parse the words.
-        line = line.strip()
-        splicedList = re.split('[ \s]',line)
+    for line in inputFile:                  # parse the words
+        splicedList = re.split(r'\W+',line)
         for  word  in  splicedList:
-            word = word.lower()
+            word =  word.lower()
+
             if word not  in wordList.keys():
-                wordList[word] = 1          #add to dict 
+                 wordList[word] = 1          #add to dict 
             else:
                 wordList[word] += 1         #add to value
+del wordList[""]
 inputFile.close()
 
 #write to the file
 with open(sys.argv[2],'w') as outputFile:
-    for x ,y in sorted(wordList.items()):
+    for x ,y in sorted(wordList.items()):  # iterate over a sorted list
         outputFile.write( "%s %d\n" % (x , y) )
 outputFile.close()
-
 print("file written")
-
-
-                    
-
-
-
-
-
-
-
-        
-         
