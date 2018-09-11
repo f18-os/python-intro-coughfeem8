0ea037e5a6213ed2ab85c1cbbedbc0e261f3740f
Sergio Yanez <seryan.1115@gmail.com>
2018-09-11 04:00:56 -0600

    added fuctionality of direcitonal commands

--- Mishelle/mishelle.py
+++ Mishelle/mishelle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env/python3
 '''
 the program is the version of shell. Will create a subprocess, include
 an ability make user redirecitons, create and run different  programs
@@ -20,22 +21,46 @@ while True:                                      #terminate with break
         take and input and read it and parse it. if you encounter 
         '<' , '>'  or '|'  you will send it to redired  option
         '''
-        command = input()
-        args = re.split(" ",command)                         #arg list
-        if args[0] == "exit":                  #myshelle exit commands
+        command = input()  
+        if command.lower()  == "exit":         #myshelle exit commands
             break
-        # try each directory in the path
-        
-        for dir in re.split(":", os.environ['PATH']): 
-            program = "%s/%s" % (dir, args[0])        
-            try:
+       
+        args = re.split(" ",command)                         #arg list
+                                               #look for piping symbol
+        symbol = re.search(r'([<>])',args)    
+        if symbol:
+            os.close(1)                      # redirect child's stdout
+            if symbol.group(1) == '>':
+              sys.stdout = open(args[2], "w")
+              fd = sys.stdout.fileno() 
+              os.set_inheritable(fd, True)
+            else:                                       # '<' is found
+              sys.stdout = open(args[0], "w")
+              fd = sys.stdout.fileno() 
+              os.set_inheritable(fd, True)  
+
+              # try each directory in path
+            for dir in re.split(":", os.environ['PATH']):
+                program = "%s/%s" % (dir,args[0])
+                try:
+                    os.execve(program, args, os.environ) #... exec program
+                except FileNotFoundError:                    # ...expected
+                    pass                                 # ...fail quietly
+                os.write(2,("It was an error running %s" %
+                            program).encode())  # terminate with error
+            continue                              
+
+        else:
+            for dir in re.split(":", os.environ['PATH']): 
+                program = "%s/%s" % (dir, args[0])        
+                try:
                                                  # try to exec program
-                os.execve(program, args, os.environ)
-            except FileNotFoundError:                       # expected
-                pass                                     #fail quietly
-            os.write(2,("It was an error running %s" %
-                        program).encode())
-            continue                                 # exit subprocess
+                    os.execve(program, args, os.environ)
+                except FileNotFoundError:                       # expected
+                    pass                                     #fail quietly
+                os.write(2,("It was an error running %s" %
+                            program).encode())
+                continue                                 # exit subprocess
 
     else:                                 #allow the parent todo stuff
         childPidCode = os.wait()
